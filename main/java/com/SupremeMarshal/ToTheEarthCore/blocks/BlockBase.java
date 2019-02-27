e3303be7817a8a2367bfbd265e9f95b496b55459
wwei23 <38965142+wwei23@users.noreply.github.com>
2019-02-27 09:33:42 -0500

    Topaz blocks power beacons

--- main/java/com/SupremeMarshal/ToTheEarthCore/blocks/BlockBase.java
+++ main/java/com/SupremeMarshal/ToTheEarthCore/blocks/BlockBase.java
@@ -64,7 +64,7 @@ public class BlockBase extends Block implements IHasModel
 	{
 		return this == net.minecraft.init.Blocks.EMERALD_BLOCK || this == ModBlocks.GOLD_BLOCK || this == ModBlocks.DIAMOND_BLOCK || this == ModBlocks.IRON_BLOCK
 				|| this == ModBlocks.SILVER_BLOCK || this == ModBlocks.PLATINUM_BLOCK || this == ModBlocks.SAPHIR_BLOCK || this == ModBlocks.FLAMING_TOPAZ_BLOCK || this == ModBlocks.RUBY_BLOCK
-				|| this == ModBlocks.AMAZONITE_BLOCK || this == ModBlocks.STEEL_BLOCK;
+				|| this == ModBlocks.AMAZONITE_BLOCK || this == ModBlocks.STEEL_BLOCK || this == ModBlocks.TOPAZ_BLOCK;
 	}
 	
 	
